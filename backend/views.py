a74d0f8b7e1d162cc3eb3a4eeeea5d29bbdf0b56
Ivansss7272 <105929547+Ivansss7272@users.noreply.github.com>
2024-08-21 10:02:57 +0200

    add new variables

--- backend/views.py
+++ backend/views.py
@@ -1,15 +1,22 @@
 @app.route('/events/bulk', methods=['POST'])
 @token_required
 def create_multiple_events(current_user):
-    data = request.get_json()
-    events = [Event(
-        title=event_data['title'],
-        description=event_data.get('description', ""),
-        date=datetime.strptime(event_data['date'], '%Y-%m-%d'),
-        organizer_id=current_user.id
-    ) for event_data in data['events']]
-    
+    events_data = request.get_json().get('events', [])  
+
+    events = []
+
+    organizer_id = current_user.id
+
+    for event_data in events_data:
+        event = Event(
+            title=event_data['title'],
+            description=event_data.get('description', ""),
+            date=datetime.strptime(event_data['date'], '%Y-%m-%d'),
+            organizer_id=organizer_id
+        )
+        events.append(event)  
+
     db.session.add_all(events)
     db.session.commit()
-    
+
     return jsonify({'message': f'{len(events)} events created successfully'}), 201
